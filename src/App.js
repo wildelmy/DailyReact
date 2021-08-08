f87be883b0261e4d0a90f3d941e1dac843835e9c
wildelmy <wildelmycolina@gmail.com>
2021-08-08 16:09:45 -0500

    crear login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../style/login.css';
+
+const Login = () => {
+  return (
+
+    <div id="container" className="alver container pt-5">
+      <div id="loader" className="m-auto"></div>
+      <div id="login" className="alver text-center">
+        <h3 className="m-3"> Iniciar Sesión</h3>
+        
+        <div className="d-grid gap-2">
+        <button type="button" className="btn btn-danger btn">
+          <b>G</b> Ingresar con Google
+        </button>
+        </div>
+
+        <hr/>
+        <div className="alver">
+        <div className="mb-3 text-start">
+      <label htmlFor="exampleInputEmail1" className="form-label">Correo electrónico</label>
+      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su correo electrónico"/>
+      </div>
+        </div>
+        <hr/>
+        <a href="#" className="text-success">¿Se te olvidó tu contraseña?</a>
+        <hr/>
+        <p>¿Aún no tienes cuenta? <a href="main.html" className="text-success">Inscribirse</a></p>
+      </div>
+    </div>
+
+    
+  )
+}
+export default Login;
