cfcfc4dc7d23a912017c2c4ae4444e6aaa4e90db
wildelmy <84979761+wildelmy@users.noreply.github.com>
2021-08-11 21:58:46 -0500

    Add files via upload

---
+++ src/questions/htmlQuestions.js
@@ -0,0 +1,19 @@
+const htmlQuestions = [
+  {
+    text: '¿Qué etiqueta es semánticamente correcta para el contenido principal?',
+    options: [
+      'Main',
+      'Section',
+      'Header'
+    ]
+  },
+  {
+    text: '¿Qué etiqueta HTML nos sirve para incluir archivos en JavaScript?',
+    options: [
+      '<br>',
+      '<script>',
+      '<styles>'
+    ]
+  }
+]
+export default htmlQuestions
